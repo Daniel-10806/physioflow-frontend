13c5e96a1ba73e09784fc5452aa302aeea5dc67a
Developer <danielj.chavarriz@gmail.com>
2026-03-22 23:39:52 -0500

    feat(physioflow): production api url

--- src/app/core/services/settings.service.ts
+++ src/app/core/services/settings.service.ts
@@ -1,64 +1,38 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+import { environment } from '../../../environment'
 
 @Injectable({
     providedIn: 'root'
 })
 export class SettingsService {
 
-    api = "http://localhost:8080/api/settings"
+    api = environment.apiUrl + "/api/settings"
 
     constructor(private http: HttpClient) { }
 
     getSettings() {
-
-        return this.http.get<any>(
-            this.api
-        )
-
+        return this.http.get<any>(this.api)
     }
 
     saveSettings(data: any) {
-
-        return this.http.post(
-            this.api,
-            data
-        )
-
+        return this.http.post(this.api, data)
     }
 
     getUsers() {
-
-        return this.http.get<any[]>(
-            this.api + "/users"
-        )
-
+        return this.http.get<any[]>(this.api + "/users")
     }
 
     addUser(data: any) {
-
-        return this.http.post(
-            this.api + "/users",
-            data
-        )
-
+        return this.http.post(this.api + "/users", data)
     }
 
     updateUser(id: number, data: any) {
-
-        return this.http.put(
-            this.api + "/users/" + id,
-            data
-        )
-
+        return this.http.put(this.api + "/users/" + id, data)
     }
 
     deleteUser(id: number) {
-
-        return this.http.delete(
-            this.api + "/users/" + id
-        )
-
+        return this.http.delete(this.api + "/users/" + id)
     }
 
 }
